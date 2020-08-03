2c8d01d06cf68980d472b53510b22d84e0e1632d
stepheku <stepheku@users.noreply.github.com>
2020-08-02 19:25:32 -0700

    Revamping api.py and using formatter black
    Began revamping api.py and created a class specific for just API calls
    also re-ran module through the formatter black
    
    Started creating tests for new class

--- kanbanflow_cli/api.py
+++ kanbanflow_cli/api.py
@@ -5,35 +5,130 @@ import json
 from datetime import datetime
 from dateutil.parser import parse
 
-
-class KanbanFlowConnector:
-    pass
+base_url = "https://kanbanflow.com/api/v1/"
 
 
-base_url = 'https://kanbanflow.com/api/v1/'
+class KanbanFlowAPICalls:
+    """
+    Contains all documented KanbanFlow API calls
+    """
+
+    def __init__(self):
+        pass
+
+    def get_board(self):
+        return get_with_api_headers(api_url_extend_path_seg(base_url, ["board"]))
+
+    def get_all_tasks(self, limit: int=20):
+        url = api_url_extend_path_seg(base_url, ["tasks"])
+        param_dict = {'limit':limit}
+
+        if not isinstance(limit, int):
+            raise TypeError('limit is an int type')
+        elif limit == 20:
+            return get_with_api_headers(url)
+        else:
+            return get_with_api_headers(
+                url_with_param(url=url, param_dict=param_dict)
+            )
+
+    def get_task_by_id(self, task_id: str) -> dict:
+        return get_with_api_headers(
+            api_url_extend_path_seg(base_url, ["tasks", task_id])
+        )
+
+    def get_tasks_by_column_id(self, column_id: str=None) -> list:
+        if column_id is not None:
+            url = api_url_extend_path_seg(base_url, ["tasks"])
+            param_dict = {'columnId': column_id}
+
+            return get_with_api_headers(
+                url_with_param(url=url, param_dict=param_dict)
+            )
+
+    def get_subtasks(self, task_id: str):
+        return get_with_api_headers(
+            api_url_extend_path_seg(base_url, ["tasks", task_id, "subtasks"])
+        )
+
+    def get_task_labels(self, task_id: str):
+        return get_with_api_headers(
+            api_url_extend_path_seg(base_url, ["tasks", task_id, "labels"])
+        )
+
+    def get_task_collaborators(self, task_id: str):
+        return get_with_api_headers(
+            api_url_extend_path_seg(base_url, ["tasks", task_id, "collaborators"])
+        )
+
+    def get_task_comments(self, task_id: str):
+        return get_with_api_headers(
+            api_url_extend_path_seg(base_url, ["tasks", task_id, "comments"])
+        )
+
+    def get_task_attachments(self, task_id: str):
+        return get_with_api_headers(
+            api_url_extend_path_seg(base_url, ["tasks", task_id, "attachments"])
+        )
+
+    def get_task_time_entries(self, task_id: str):
+        return get_with_api_headers(
+            api_url_extend_path_seg(base_url, ["tasks", task_id, "time-entries"])
+        )
+
+    def get_task_manual_time_entry(self, task_id: str, manual_time_entry_id: str):
+        return get_with_api_headers(
+            api_url_extend_path_seg(
+                base_url,
+                ["tasks", task_id, "manual-time-entries", manual_time_entry_id],
+            )
+        )
+
+    def get_task_relations(self, task_id: str):
+        return get_with_api_headers(
+            api_url_extend_path_seg(base_url, ["tasks", task_id, "relations"])
+        )
+
+    def get_users(self):
+        return get_with_api_headers(api_url_extend_path_seg(base_url, ["users"]))
+
+
+def api_url_extend_path_seg(base_url: str, path_segments: list) -> str:
+    """
+    Given a list of path segments to extend on a base url, this will 
+    return the extended path segment
+    """
+    if not isinstance(path_segments, list):
+        raise TypeError("Argument path_segments must be a list type")
+    elif not all(isinstance(n, str) for n in path_segments):
+        raise TypeError("All path segments must be a string type")
+
+    if base_url[-1] != "/":
+        base_url += "/"
+
+    return base_url + "/".join(path_segments)
+
 
 task_valid_parameters = [
-    'columnId',
-    'columnIndex',
-    'columnName',
-    'startTaskId',
-    'startGroupingDate',
-    'limit',
-    'order',
+    "columnId",
+    "columnIndex",
+    "columnName",
+    "startTaskId",
+    "startGroupingDate",
+    "limit",
+    "order",
 ]
 
 
 def api_key_b64_header(api_key: str) -> dict:
     if not api_key:
-        raise(EnvironmentError('Missing API key from function'))
+        raise (EnvironmentError("Missing API key from function"))
     elif not isinstance(api_key, str):
-        raise(AttributeError('API key must be in a String format'))
+        raise (AttributeError("API key must be in a String format"))
     else:
-        api_key = api_key.encode('utf-8')
-        api_key_b64 = base64.b64encode(b'apiToken:' + api_key)
-        headers = {
-            'Authorization': 'Basic {}'.format(api_key_b64.decode('utf-8'))
-        }
+        api_key = api_key.encode("utf-8")
+        api_key_b64 = base64.b64encode(b"apiToken:" + api_key)
+        headers = {"Authorization": "Basic {}".format(api_key_b64.decode("utf-8"))}
         return headers
 
 
@@ -42,7 +137,7 @@ def get_with_api_headers(url: str) -> dict:
     Runs a GET with headers per KanbanFlow API
     :param url: URL to be retrieved
     """
-    api_key = os.environ.get('KBFLOW_API')
+    api_key = os.environ.get("KBFLOW_API")
     headers = api_key_b64_header(api_key)
     return json.loads(requests.get(url, headers=headers).text)
 
@@ -54,9 +149,9 @@ def post_with_api_headers(url: str, data: dict) -> requests.post:
     :param url: URL to be retrieved
     :param data: Data to be posted in a dictionary
     """
-    api_key = os.environ.get('KBFLOW_API')
+    api_key = os.environ.get("KBFLOW_API")
     headers = api_key_b64_header(api_key)
-    headers['Content-type'] = 'application/json'
+    headers["Content-type"] = "application/json"
     return requests.post(url=url, json=data, headers=headers)
 
 
@@ -71,9 +166,11 @@ def url_with_param(url: str, param_dict: dict=None) -> str:
                                     for (k, v) in param_dict.items())
 
 
-def check_mandatory_fields(mandatory_fields: list = None,
-                           or_mandatory_fields: list = None,
-                           local_vars: dict = None) -> list:
+def check_mandatory_fields(
+    mandatory_fields: list = None,
+    or_mandatory_fields: list = None,
+    local_vars: dict = None,
+) -> list:
     keys_without_values = []
     if mandatory_fields:
         for k, v in local_vars.items():
@@ -82,8 +179,13 @@ def check_mandatory_fields(mandatory_fields: list = None,
         if not keys_without_values:
             return True
         else:
-            raise(AttributeError('Mandatory fields are blank: {}'.format(
-                ', '.join(keys_without_values))))
+            raise (
+                AttributeError(
+                    "Mandatory fields are blank: {}".format(
+                        ", ".join(keys_without_values)
+                    )
+                )
+            )
     elif or_mandatory_fields:
         for k, v in local_vars.items():
             if k in or_mandatory_fields and v is not None:
@@ -91,13 +193,17 @@ def check_mandatory_fields(mandatory_fields: list = None,
         if set(or_mandatory_fields) != set(keys_without_values):
             return True
         else:
-            raise(AttributeError('One of the mandatory fields are blank: {}'.format(
-                ', '.join(keys_without_values))))
+            raise (
+                AttributeError(
+                    "One of the mandatory fields are blank: {}".format(
+                        ", ".join(keys_without_values)
+                    )
+                )
+            )
 
 
-def format_group_date(datetime_str: str = None, 
-                      datetime_obj: datetime = None) -> str:
-    datetime_format = '%Y-%m-%d'
+def format_group_date(datetime_str: str = None, datetime_obj: datetime = None) -> str:
+    datetime_format = "%Y-%m-%d"
     if datetime_str:
         return parse(datetime_str).strftime(datetime_format)
     elif datetime_obj:
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,26 +1,50 @@
 import unittest
-from kanbanflow_cli.kanban_board import KanbanBoard
+from kanbanflow_cli.api import KanbanFlowAPICalls
+import configparser
+from pathlib import Path
+import os
+
+api_caller = KanbanFlowAPICalls()
+file_path = Path(os.path.dirname(os.path.abspath(__file__)))
 
 
-class TestKanbanBoardAPICalls(unittest.TestCase):
-    board = KanbanBoard()
+class TestAPICalls(unittest.TestCase):
+    def setUp(self):
+        config = configparser.ConfigParser()
+        config_file_path = os.path.abspath(
+            os.path.join(file_path.parents[0], "config.ini")
+        )
+        config.read(os.path.join(config_file_path))
+        os.environ["KBFLOW_API"] = config["api_key"]["kbflow_api_key"]
 
-    def test_board_api_url(self, board_obj=board):
-        self.assertEqual(
-            'https://kanbanflow.com/api/v1/board', board_obj.api_url)
+        self.all_tasks = api_caller.get_all_tasks(limit=1)
+        self.sample_task_id = self.all_tasks[0].get("tasks")[0].get("_id")
+        self.sample_column_id = self.all_tasks[0].get("columnId")
 
-    def test_board_users_api_url(self, board_obj=board):
-        self.assertEqual('https://kanbanflow.com/api/v1/users',
-                         board_obj.users_api_url)
+    def test_get_board(self):
+        """
+        get_board() method returns a valid dict
+        """
+        board_json = api_caller.get_board()
+        self.assertIsInstance(board_json, dict)
 
-    def test_board_json_is_dict(self, board_obj=board):
-        self.assertIsInstance(board_obj.board_json, dict)
+    def test_get_task_by_id(self):
+        """
+        sample task_id returns a dict with the task_id
+        """
+        task_json = api_caller.get_task_by_id(task_id=self.sample_task_id)
+        self.assertEqual(task_json.get("_id"), self.sample_task_id)
 
-    def test_board_users_json_is_list(self, board_obj=board):
-        self.assertIsInstance(board_obj.board_users_json, list)
+    def test_get_tasks_by_column_id(self):
+        """
+        sample column_id returns a list of tasks
+        """
+        task_list = api_caller.get_tasks_by_column_id(
+            column_id=self.sample_column_id)
+        self.assertIsInstance(task_list, list)
 
-    def test_board_users_json_more_than_zero(self, board_obj=board):
-        self.assertGreater(len(board_obj.board_users_json), 0)
+    def tearDown(self):
+        del os.environ["KBFLOW_API"]
 
 
 if __name__ == "__main__":
